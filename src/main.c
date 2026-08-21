a5a5283800a07e6b8d2f20b9a1e38ef3e8dae3d6
Rafin010 <1.11105988e+08+Rafin010@users.noreply.github.com>
2026-08-21 14:25:52 +0000

    Add starter project

---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("File Compression Tool starter\n");
+    printf("Next step: implement binary files and a simple compression algorithm.\n");
+    return 0;
+}
